ccd7695126ca4bc8e3ea16e787aaee8ea4f88239
rsdharmaji <59861195+rsdharmaji@users.noreply.github.com>
2022-02-16 15:33:08 -0800

    rd - add earthquake table + some tests

---
+++ frontend/src/fixtures/earthquakesFixtures.js
@@ -0,0 +1,35 @@
+const earthquakesFixtures = {
+    oneEarthquake: {
+        "id": 1,
+        "title": "M 2.2 - 10km ESE of Ojai, CA",
+        "mag": 2.16,
+        "place": "10km ESE of Ojai, CA",
+        "time": 1644571919000
+    },
+    threeEarthquakes: [
+        {
+            "id": 1,
+            "title": "M 2.2 - 10km ESE of Ojai, CA",
+            "mag": 2.16,
+            "place": "10km ESE of Ojai, CA",
+            "time": 1644571919000
+        },
+        {
+            "id": 2,
+            "title": "M 3.3 - 10km ESE of Ojai, CA",
+            "mag": 3.16,
+            "place": "10km ESE of Ojai, CA",
+            "time": 1644571919020
+        },
+        {
+            "id": 3,
+            "title": "M 4.4 - 10km ESE of Ojai, CA",
+            "mag": 4.16,
+            "place": "10km ESE of Ojai, CA",
+            "time": 1644571919030
+        }
+    ]
+};
+
+
+export { earthquakesFixtures };
