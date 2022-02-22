c37e490badaf47a7c0cbec180c71a513aeeaa4dd
Joshua Kim <skim945@umail.ucsb.edu>
2022-02-21 22:49:31 -0800

    jk-added the create page need to fix inactive feature

--- frontend/src/main/pages/UCSBSubjects/UCSBSubjectsCreatePage.js
+++ frontend/src/main/pages/UCSBSubjects/UCSBSubjectsCreatePage.js
@@ -1,13 +1,51 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import UCSBSubjectsForm from "main/components/UCSBSubjects/UCSBSubjectsForm";
+import { Navigate } from 'react-router-dom'
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
+
 
 export default function UCSBSubjectsCreatePage() {
+  const objectToAxiosParams = (ucsbSubjects) => ({
+    url: "/api/ucsbsubjects/post",
+    method: "POST",
+    params: {
+      subjectCode: ucsbSubjects.subjectCode,
+      subjectTranslation: ucsbSubjects.subjectTranslation,
+      collegeCode: ucsbSubjects.collegeCode,
+      deptCode: ucsbSubjects.deptCode,
+      relatedDeptCode: ucsbSubjects.relatedDeptCode,
+      inactive: ucsbSubjects.inactive
+    }
+  });
+
+  const onSuccess = (ucsbSubjects) => {
+    toast(`New ucsbSubjects Created - id: ${ucsbSubjects.id} subjectCode: ${ucsbSubjects.subjectCode}`);
+  }
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+     { onSuccess }, 
+     // Stryker disable next-line all : hard to set up test for caching
+     ["/api/ucsbSubjects/all"]
+     );
+
+     const { isSuccess } = mutation
+
+     const onSubmit = async (data) => {
+       mutation.mutate(data);
+     }
+   
+     if (isSuccess) {
+       return <Navigate to="/ucsbsubjects/list" />
+     }
     return (
       <BasicLayout>
         <div className="pt-2">
           <h1>Create New UCSBSubject</h1>
-          <p>
-            This is where the create page will go
-          </p>
+
+          <UCSBSubjectsForm submitAction={onSubmit} />
+
         </div>
       </BasicLayout>
     )
